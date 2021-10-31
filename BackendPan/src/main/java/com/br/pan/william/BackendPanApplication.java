f418bc90d9e7a2621837970e7b769a559c0dc56c
William F Assis <willifran@gmail.com>
2021-10-30 21:16:26 -0300

    iniciando projeto

---
+++ BackendPan/src/main/java/com/br/pan/william/BackendPanApplication.java
@@ -0,0 +1,13 @@
+package com.br.pan.william;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPanApplication.class, args);
+	}
+
+}
